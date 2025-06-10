3b452bdb39756fddc61856670dcc7a5f44f9dfcd
Ralph <pok12369@naver.com>
2025-06-10 22:25:06 +0900

    에러 핸들러에 토큰 리프레시

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,11 +1,12 @@
-import { AxiosError } from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import { CustomError } from "./custom-error";
+import instance from "@/apis/custom-instance";
 
 export interface ErrorResponse {
   message: string;
 }
 
-export const errorHandler = (error: AxiosError) => {
+export const errorHandler = async (error: AxiosError) => {
   const response = error.response;
   if (!response) {
     return Promise.reject(new CustomError(0, "응답이 없습니다.", "external"));
@@ -29,8 +30,33 @@ export const errorHandler = (error: AxiosError) => {
       customError.setCode("INVALID_REQUEST");
       break;
     case 401:
-      customError.setUserMessage("로그인이 필요합니다.");
-      customError.setCode("EXPIRED_TOKEN");
+      try {
+        const refreshToken = "your-refresh-token";
+        if (refreshToken) {
+          // #. 인스턴스가 아니라 액시오스를 타야 무한 호출을 방지함.
+          const {
+            data: { data: token },
+          }: { data: { accessToken; refreshToken } } = await axios.post(
+            `/user/refresh?refreshToken=${refreshToken}`
+          );
+
+          const retryRequest = {
+            ...error.config,
+            headers: {
+              ...error.config?.headers,
+              Authorization: `Bearer ${accessToken}`,
+            },
+          } as AxiosRequestConfig;
+          return await instance.request(retryRequest);
+        }
+      } catch (e) {
+        console.error("Token refresh Failed", e);
+        // #. 에러 실패 로직 주입
+        // #. 최종 실패이므로, 여기서 라우터 사용 등 실제 처리 진행
+        customError.setUserMessage("로그인이 필요합니다.");
+        customError.setCode("EXPIRED_TOKEN");
+      }
+
       break;
     case 404:
       customError.setUserMessage("요청한 리소스를 찾을 수 없습니다.");
